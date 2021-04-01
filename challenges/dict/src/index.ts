3341eb3be24b67450e2e9a98d589fd735b729bbf
wscnd <wellingson@gmail.com>
2021-04-01 14:17:55 -0300

    chore: generics exercise done mapDict

--- challenges/dict/src/index.ts
+++ challenges/dict/src/index.ts
@@ -1,7 +1,28 @@
-export type Dict = {};
+export type Dict<T> = {
+    [k: string]: T | undefined;
+};
 
 // Array.prototype.map, but for Dict
-export function mapDict() {}
+export function mapDict<T, S>(
+    dict: Dict<T>,
+    fn: (currentValue: T) => S
+): Dict<S> {
+    const out: Dict<S> = {};
+    Object.keys(dict).forEach(currentValue => {
+        const item = dict[currentValue];
+
+        if (typeof item !== 'undefined') {
+            out[currentValue] = fn(item);
+        }
+    });
+    return out;
+}
+
+mapDict({
+    a: 'a',
+    b: 'b',
+    c: 'c'
+}, str => ({ val: str }));
 
 // Array.prototype.reduce, but for Dict
-export function reduceDict() {}
+export function reduceDict<T>(dict: Dict<T>) { }
